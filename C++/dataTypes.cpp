05e8fddb19f94f7c8c957d4e85c5f5e511211615
Tom Simpson <tom@thesimpsonhouse.co.uk>
2022-10-11 17:33:24 +0100

    Re-organising files

---
+++ C++/dataTypes.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char grade = 'A';
+	string subject = "Science";
+	int mark = 34;
+	double percentage = 85.6;
+	bool ispassed = true;	
+}
+
+
---
+++ C++/variables.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	string characterName = "Bill";
+	int characterAge = 70;
+
+	cout << "There was once a man called " << characterName <<  endl;
+	cout << "He was " << characterAge << " years old" << endl;
+	cout << "He liked the name " << characterName << endl;
+	cout << "But did not like being " << characterAge << endl;
+}
+
+
